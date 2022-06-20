cd90aae022cfda39bc41fc7cb202300e371afd8e
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-21 01:44:03 +0800

    添加两个全局变量const, global，分别对应K，G

---
+++ script/manager/const.gd
@@ -0,0 +1,47 @@
+extends Node
+
+const CLOUDS = {
+	"Day":
+	[
+		"res://texture/cloud/Cloud Day 01.png",
+		"res://texture/cloud/Cloud Day 02.png",
+		"res://texture/cloud/Cloud Day 03.png",
+		"res://texture/cloud/Cloud Day 04.png",
+		"res://texture/cloud/Cloud Day 05.png",
+		"res://texture/cloud/Cloud Day 06.png",
+		"res://texture/cloud/Cloud Day 07.png",
+		"res://texture/cloud/Cloud Day 08.png",
+		"res://texture/cloud/Cloud Day 09.png",
+		"res://texture/cloud/Cloud Day 10.png",
+		"res://texture/cloud/Cloud Day 11.png",
+		"res://texture/cloud/Cloud Day 12.png",
+		"res://texture/cloud/Cloud Day 13.png",
+		"res://texture/cloud/Cloud Day 14.png",
+		"res://texture/cloud/Cloud Day 15.png",
+		"res://texture/cloud/Cloud Day 16.png",
+		"res://texture/cloud/Cloud Day 17.png",
+		"res://texture/cloud/Cloud Day 18.png",
+		"res://texture/cloud/Cloud Day 19.png",
+		"res://texture/cloud/Cloud Day 20.png",
+		"res://texture/cloud/Cloud Day 21.png",
+		"res://texture/cloud/Cloud Day 22.png",
+		"res://texture/cloud/Cloud Day 23.png",
+		"res://texture/cloud/Cloud Day 24.png",
+		"res://texture/cloud/Cloud Day 25.png",
+		"res://texture/cloud/Cloud Day 26.png",
+		"res://texture/cloud/Cloud Day 27.png",
+		"res://texture/cloud/Cloud Day 28.png",
+		"res://texture/cloud/Cloud Day 29.png",
+		"res://texture/cloud/Cloud Day 30.png",
+	],
+	"Night":
+	[
+		"res://texture/cloud/Cloud Night 01.png",
+		"res://texture/cloud/Cloud Night 02.png",
+		"res://texture/cloud/Cloud Night 03.png",
+		"res://texture/cloud/Cloud Night 04.png",
+		"res://texture/cloud/Cloud Night 05.png",
+		"res://texture/cloud/Cloud Night 06.png",
+		"res://texture/cloud/Cloud Night 07.png",
+	]
+}
---
+++ script/manager/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var cloudUsed = {}
+
+
+func _ready():
+	cloudUsed.clear()
