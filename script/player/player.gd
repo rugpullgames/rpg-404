ea9fa222345314e5400d2e5a3a5e6daf559ea9fd
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-22 00:49:17 +0800

    Player加载Emo的属性

---
+++ script/player/player.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+# node
+onready var SprEmo: Sprite = $SprEmo
+
+
+func _ready():
+	__bind_events()
+
+
+func __bind_events():
+	var error_code = Events.connect("update_traits", self, "__reset")
+	assert(error_code == OK, error_code)
+
+
+func __reset():
+	__reset_emo_type()
+
+
+func __reset_emo_type():
+	if not MgrNft.NFT_TRAITS or not MgrNft.NFT_TRAITS.emo:
+		push_warning("Wrong NFT emo traits.")
+		return
+	var res = "res://texture/emo/%s.png" % [MgrNft.NFT_TRAITS.emo]
+	print(res)
+	SprEmo.texture = load(res)
