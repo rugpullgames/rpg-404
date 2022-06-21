796a1e2b9cfc9c7705cfcfff0bcad97fdd66cce1
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-22 03:24:11 +0800

    更新Player和BaseFloor：添加碰撞组，实现Player跳跃

--- script/player/player.gd
+++ script/player/player.gd
@@ -1,8 +1,23 @@
-extends Node2D
+extends KinematicBody2D
 
-# node
-onready var SprEmo: Sprite = $SprEmo
+const GRAVITY = 1500
+const JUMP_FORCE = 450
+
+var velocity = Vector2()
+var screen_size
 
 
 func _ready():
-	pass
+	screen_size = get_viewport_rect().size
+
+
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
+		velocity.y = -JUMP_FORCE
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+	# prevent player going out of screen
+	position.y = clamp(position.y, 0, screen_size.y)
