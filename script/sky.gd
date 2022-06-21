ca64c8c3084f6d341eaa843f24991b59d6381fee
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-21 10:24:02 +0800

    添加天空颜色的读取NFT并reset

---
+++ script/sky.gd
@@ -0,0 +1,23 @@
+extends ColorRect
+
+
+func _ready():
+	__bind_events()
+
+
+func __bind_events():
+	var error_code = Events.connect("update_traits", self, "__reset")
+	assert(error_code == OK, error_code)
+
+
+func __reset():
+	if not MgrNft.NFT_TRAITS or not MgrNft.NFT_TRAITS.background:
+		push_warning("Wrong NFT sky trait.")
+		return
+
+	if not K.DATA_BACKGROUND.has(MgrNft.NFT_TRAITS.background):
+		push_warning("Not found background id, " + MgrNft.NFT_TRAITS.background)
+		return
+
+	var skyColor = K.DATA_BACKGROUND.get(MgrNft.NFT_TRAITS.background).sky_color
+	self.color  = Color(skyColor)
