32c72594fd2131f5843412a1d5e49b9aeeb69f0b
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-21 09:20:03 +0800

    添加测试用的nft_metadata同时从metadata中提取traits形成kv pairs

--- script/manager/mgr_nft.gd
+++ script/manager/mgr_nft.gd
@@ -5,7 +5,34 @@
 
 extends Node
 
-var NFT_META = null
+const NFT_META_TEST = {
+	"attributes":
+	[
+		{"trait_type": "Background", "value": "Mid Blue Sky 01"},
+		{"trait_type": "Foreground", "value": "Base Floor"},
+		{"trait_type": "Barrier", "value": "Dead Goblins"},
+		{"trait_type": "Floor", "value": "Floor 29"},
+		{"trait_type": "Pants", "value": "Pants 13"},
+		{"trait_type": "Jacket", "value": "Jacket 10"},
+		{"trait_type": "Head", "value": "Head 16"},
+		{"trait_type": "Weapon", "value": "Weapon 09"},
+		{"trait_type": "Emo", "value": "Emo 07"},
+		{"trait_type": "Pet", "value": "Space Ship"}
+	],
+	"date": 1655407034751,
+	"description":
+	"The first game of Rug Pull Games, RPG 404. Please visit rpg404.com to play this tiny game.",
+	"dna": "94b508951b0b9b574224d15120a6c97adb898cfb",
+	"edition": 1,
+	"external_url": "https://rpg404.com/",
+	"image": "ipfs://QmXg2eD8YNcfADVhpnci7SXCQrN1h1m1woPWh8hD8mUAkS/1.png",
+	"name": "RPG404 #1",
+	"twitter": "https://twitter.com/rug_pull_games",
+	"website": "https://rug-pull.games/"
+}
+
+var NFT_META = NFT_META_TEST  # Metadata JSON
+var NFT_TRAITS = null  # Traits Dict
 
 
 func _ready():
@@ -20,7 +47,7 @@ func load_nft_metadata():
 			console.log('The JavaScript singleton is available')
 			var strMetadata = JSON.stringify(window.nftMetadata)
 			strMetadata
-		    """
+			"""
 		)
 		if nftData and not nftData.empty():
 			var p = JSON.parse(nftData)
@@ -32,3 +59,20 @@ func load_nft_metadata():
 			push_error("Metadata is NULL.")
 	else:
 		print("The JavaScript singleton is NOT available")
+
+	get_traits()
+
+
+func get_traits():
+	if not NFT_META:
+		return
+
+	NFT_TRAITS = {}
+	for trait in NFT_META.attributes:
+		NFT_TRAITS[trait.trait_type] = trait.value
+
+	print(NFT_TRAITS)
+
+
+func update_metadata_traits():
+	pass
