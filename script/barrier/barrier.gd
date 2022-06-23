07c413128c739e979f2fede7ef5563fcce325615
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-23 22:27:35 +0800

    barrier生成

--- script/barrier/barrier.gd
+++ script/barrier/barrier.gd
@@ -5,18 +5,36 @@
 
 extends Area2D
 
-# const 
+# node
+onready var SprBarrier: Sprite = $SprBarrier
+
+# const
 const SCREEN_WIDTH = 900
-const DEFAULT_POS_X = SCREEN_WIDTH * 2
-const SPEED_X = 200
+const DEFAULT_POS_X = SCREEN_WIDTH + 100
+const SPEED_X = 300
 
-func _ready():
-    __bind_events()
-
-func __bind_events():
-    var error_code = Events.connect("update_traits", self, "__reset")
-	assert(error_code == OK, error_code)
+# local var
+var moving = false
 
 
-func __reset():
-    pass
+func reset(texture):
+	if not texture:
+		push_warning("Barrier texture is null.")
+		return
+
+	SprBarrier.texture = texture
+	moving = true
+	self.visible = true
+	self.position.x = DEFAULT_POS_X
+
+
+func _physics_process(dt):
+	if moving:
+		__move(dt)
+
+
+func __move(dt):
+	self.position.x -= SPEED_X * dt
+	if self.position.x < -SCREEN_WIDTH - 100:
+		moving = false
+		self.visible = false
--- script/barrier/barrier_spawner.gd
+++ script/barrier/barrier_spawner.gd
@@ -1,16 +1,63 @@
 extends Node
 
+# const
+const SPAWN_TIME_MIN = 2
+const SPAWN_TIME_MAX = 3
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# local var
+var tt = 0
+var nextTime = rand_range(SPAWN_TIME_MIN, SPAWN_TIME_MAX)
+var textures = []
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	__bind_events()
+
+
+func __bind_events():
+	var error_code = Events.connect("update_traits", self, "__reset")
+	assert(error_code == OK, error_code)
+
+
+func __reset():
+	__reset_barrier_textures()
+	__disable_all_barriers()
+
+
+func __reset_barrier_textures():
+	if not MgrNft.NFT_TRAITS or not MgrNft.NFT_TRAITS.barrier:
+		push_warning("Wrong NFT cloud traits.")
+		return
+
+	textures = []
+	for n in range(1, 6):
+		print(n)
+		var res = (
+			"res://texture/barrier/%s/%s 0%s.png"
+			% [MgrNft.NFT_TRAITS.barrier, MgrNft.NFT_TRAITS.barrier, n]
+		)
+		var texture = load(res)
+
+		textures.append(texture)
+
+
+func __disable_all_barriers():
+	for brr in self.get_children():
+		brr.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(dt):
+	tt += dt
+	if tt >= nextTime:
+		__spawn_barrier()
+		tt = 0
+		nextTime = rand_range(SPAWN_TIME_MIN, SPAWN_TIME_MAX)
+
+
+func __spawn_barrier():
+	print("spawn")
+	for brr in self.get_children():
+		if not brr.visible:
+			brr.reset(textures[0])
+			break
