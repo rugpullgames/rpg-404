2fe37538013d3d4f67f15baad057cb1456826aaf
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-25 15:05:36 +0800

    barrier的sprite和碰撞的位置调整

--- script/barrier/barrier.gd
+++ script/barrier/barrier.gd
@@ -7,14 +7,22 @@ extends Area2D
 
 # node
 onready var SprBarrier: Sprite = $SprBarrier
+onready var CollsionShape: CollisionShape2D = $CollisionShape2D
 
 # const
 const SCREEN_WIDTH = 900
 const DEFAULT_POS_X = SCREEN_WIDTH + 100
 const SPEED_X = 300
+const GRAVITY = 1500
 
 # local var
+var velocity = Vector2()
 var moving = false
+var shape2d: RectangleShape2D
+
+
+func _ready():
+	shape2d = CollsionShape.shape as RectangleShape2D
 
 
 func reset(texture):
@@ -22,7 +30,13 @@ func reset(texture):
 		push_warning("Barrier texture is null.")
 		return
 
+	var textureSize = texture.get_size()
 	SprBarrier.texture = texture
+	SprBarrier.offset = -textureSize
+	CollsionShape.position.x = -textureSize.x * 2.5
+	CollsionShape.position.y = -textureSize.y * 2
+	shape2d.extents = textureSize / 2
+
 	moving = true
 	self.visible = true
 	self.position.x = DEFAULT_POS_X
@@ -34,6 +48,8 @@ func _physics_process(dt):
 
 
 func __move(dt):
+	velocity.y += dt * GRAVITY
+
 	self.position.x -= SPEED_X * dt
 	if self.position.x < -SCREEN_WIDTH - 100:
 		moving = false
