0ae1fdba68ba5df0fd8b467a710dc27c53e7cc86
Yuancheng Zhang <old.far.hot@gmail.com>
2022-07-04 05:09:43 +0800

    计分功能

--- script/map/map_main.gd
+++ script/map/map_main.gd
@@ -5,7 +5,28 @@
 
 extends Node
 
+# node
+onready var ScoreMarker: Node2D = $Game/ScoreMarker
+
+# const
+const SPEED_X = K.SPEED_X
+const SCORE_FACTOR: float = 1.0 / 20
+
 
 func _ready():
 	MgrNft.reload_nft()
+	__reset()
 	print("Game Start")
+
+
+func __reset():
+	ScoreMarker.position = Vector2.ZERO
+
+
+func _physics_process(dt):
+	__update_marker(dt)
+
+
+func __update_marker(dt):
+	ScoreMarker.position.x -= SPEED_X * dt
+	G.score = abs(floor(ScoreMarker.position.x * SCORE_FACTOR))
