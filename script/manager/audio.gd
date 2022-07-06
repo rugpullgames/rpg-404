c89b22655b90e3caebb90c656736ff9c18ab365b
Yuancheng Zhang <old.far.hot@gmail.com>
2022-07-07 04:24:02 +0800

    添加批注

--- script/manager/audio.gd
+++ script/manager/audio.gd
@@ -1,3 +1,8 @@
+# @copyright Rug Pull Games
+# @see Rug Pull Games: https://rug-pull.games/
+# @see RPG 404: https://rpg404.com/
+# @author endaye.eth, Fried Egg Fendi
+
 extends Control
 
 onready var BtnBgm: Button = $BtnBgmAudio
