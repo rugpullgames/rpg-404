c3ea2665f9a1a5cd5bf9a44ecf350078c9bce755
Yuancheng Zhang <old.far.hot@gmail.com>
2022-07-07 05:49:54 +0800

    添加version

---
+++ script/manager/mgr_version.gd
@@ -0,0 +1,13 @@
+# Version
+# @see Hidden Moss: https://hiddenmoss.com/
+# @see Miss. Peacock: https://miss-peacock.com/
+# @author Yuancheng Zhang
+
+extends Node
+
+# Version
+const MAJOR = 1
+const MINOR = 0
+const BUGFIX = 0
+const COMMIT = 567503
+const VERSION = "v%s.%s.%s.%s" % [MAJOR, MINOR, BUGFIX, COMMIT]
