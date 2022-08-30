667a2441b948ba41a22c87004c108c68c42f4f2a
Yuancheng Zhang <old.far.hot@gmail.com>
2022-08-30 09:59:08 +0800

    修复sky、cloud、star相关引用; [main] v1.1.4.618247

--- script/manager/const.gd
+++ script/manager/const.gd
@@ -19,67 +19,86 @@ const GameState = {INIT = 0, READY = 1, RUNNING = 2, END = 3}
 # collision layers
 enum CollsionLayer { BASEFLOOR = 0, PLAYER, BARRIER, PET }
 
-const DATA_BACKGROUND = preload("res://script/data/data_background.gd").background
-const DATA_MUSIC = preload("res://script/data/data_music.gd").music
+const DATA_BACKGROUND_RAW = preload("res://script/data/data_background.gd").background
+const DATA_MUSIC_RAW = preload("res://script/data/data_music.gd").music
+const DATA_BACKGROUND = {}
+const DATA_MUSIC = {}
 
 const CLOUDS = {
 	"Day":
 	[
-		"res://texture/cloud/Cloud Day 01.png",
-		"res://texture/cloud/Cloud Day 02.png",
-		"res://texture/cloud/Cloud Day 03.png",
-		"res://texture/cloud/Cloud Day 04.png",
-		"res://texture/cloud/Cloud Day 05.png",
-		"res://texture/cloud/Cloud Day 06.png",
-		"res://texture/cloud/Cloud Day 07.png",
-		"res://texture/cloud/Cloud Day 08.png",
-		"res://texture/cloud/Cloud Day 09.png",
-		"res://texture/cloud/Cloud Day 10.png",
-		"res://texture/cloud/Cloud Day 11.png",
-		"res://texture/cloud/Cloud Day 12.png",
-		"res://texture/cloud/Cloud Day 13.png",
-		"res://texture/cloud/Cloud Day 14.png",
-		"res://texture/cloud/Cloud Day 15.png",
-		"res://texture/cloud/Cloud Day 16.png",
-		"res://texture/cloud/Cloud Day 17.png",
-		"res://texture/cloud/Cloud Day 18.png",
-		"res://texture/cloud/Cloud Day 19.png",
-		"res://texture/cloud/Cloud Day 20.png",
-		"res://texture/cloud/Cloud Day 21.png",
-		"res://texture/cloud/Cloud Day 22.png",
-		"res://texture/cloud/Cloud Day 23.png",
-		"res://texture/cloud/Cloud Day 24.png",
-		"res://texture/cloud/Cloud Day 25.png",
-		"res://texture/cloud/Cloud Day 26.png",
-		"res://texture/cloud/Cloud Day 27.png",
-		"res://texture/cloud/Cloud Day 28.png",
-		"res://texture/cloud/Cloud Day 29.png",
-		"res://texture/cloud/Cloud Day 30.png",
+		"res://texture/cloud/cloud_day_01.png",
+		"res://texture/cloud/cloud_day_02.png",
+		"res://texture/cloud/cloud_day_03.png",
+		"res://texture/cloud/cloud_day_04.png",
+		"res://texture/cloud/cloud_day_05.png",
+		"res://texture/cloud/cloud_day_06.png",
+		"res://texture/cloud/cloud_day_07.png",
+		"res://texture/cloud/cloud_day_08.png",
+		"res://texture/cloud/cloud_day_09.png",
+		"res://texture/cloud/cloud_day_10.png",
+		"res://texture/cloud/cloud_day_11.png",
+		"res://texture/cloud/cloud_day_12.png",
+		"res://texture/cloud/cloud_day_13.png",
+		"res://texture/cloud/cloud_day_14.png",
+		"res://texture/cloud/cloud_day_15.png",
+		"res://texture/cloud/cloud_day_16.png",
+		"res://texture/cloud/cloud_day_17.png",
+		"res://texture/cloud/cloud_day_18.png",
+		"res://texture/cloud/cloud_day_19.png",
+		"res://texture/cloud/cloud_day_20.png",
+		"res://texture/cloud/cloud_day_21.png",
+		"res://texture/cloud/cloud_day_22.png",
+		"res://texture/cloud/cloud_day_23.png",
+		"res://texture/cloud/cloud_day_24.png",
+		"res://texture/cloud/cloud_day_25.png",
+		"res://texture/cloud/cloud_day_26.png",
+		"res://texture/cloud/cloud_day_27.png",
+		"res://texture/cloud/cloud_day_28.png",
+		"res://texture/cloud/cloud_day_29.png",
+		"res://texture/cloud/cloud_day_30.png",
 	],
 	"Night":
 	[
-		"res://texture/cloud/Cloud Night 01.png",
-		"res://texture/cloud/Cloud Night 02.png",
-		"res://texture/cloud/Cloud Night 03.png",
-		"res://texture/cloud/Cloud Night 04.png",
-		"res://texture/cloud/Cloud Night 05.png",
-		"res://texture/cloud/Cloud Night 06.png",
-		"res://texture/cloud/Cloud Night 07.png",
+		"res://texture/cloud/cloud_night_01.png",
+		"res://texture/cloud/cloud_night_02.png",
+		"res://texture/cloud/cloud_night_03.png",
+		"res://texture/cloud/cloud_night_04.png",
+		"res://texture/cloud/cloud_night_05.png",
+		"res://texture/cloud/cloud_night_06.png",
+		"res://texture/cloud/cloud_night_07.png",
 	]
 }
 
 const STARS = [
-	"res://texture/stars/Star 01.png",
-	"res://texture/stars/Star 02.png",
-	"res://texture/stars/Star 03.png",
-	"res://texture/stars/Star 04.png",
+	"res://texture/stars/star_01.png",
+	"res://texture/stars/star_02.png",
+	"res://texture/stars/star_03.png",
+	"res://texture/stars/star_04.png",
 ]
 
 
 func _ready():
+	_init_screen()
+	_clean_data()
+
+
+func _init_screen():
 	get_tree().set_screen_stretch(
 		SceneTree.STRETCH_MODE_2D,
 		SceneTree.STRETCH_ASPECT_KEEP,
 		Vector2(SCREEN_WIDTH, SCREEN_HEIGHT),
 		1.0
 	)
+
+
+func _clean_data():
+	_clean_data_aux(DATA_BACKGROUND, DATA_BACKGROUND_RAW)
+	_clean_data_aux(DATA_MUSIC, DATA_MUSIC_RAW)
+
+
+func _clean_data_aux(tar, raw):
+	for key in raw:
+		var id = key.to_lower().replace(" ", "_")
+		tar[id] = raw[key]
+		tar[id].id = id
