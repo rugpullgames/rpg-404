df0cf3fa33eda95d1f6ef6d85b74d9d1077bfeba
Yuancheng Zhang <old.far.hot@gmail.com>
2022-09-18 03:09:45 +0800

    从网络上加载emo; [main] v2.1.2.634417

--- script/player/player_emo.gd
+++ script/player/player_emo.gd
@@ -7,7 +7,12 @@ extends Sprite
 
 const SHOW_TIME = 0.3
 
+const TMP_RPG404_EMO_FILE = "user://rpg404_emo.png"
+
 var _tt = 0
+
+# node
+onready var HTTPRequest: HTTPRequest = $HTTPRequest
 
 ### default
 
@@ -58,7 +63,17 @@ func _reset() -> void:
 
 func _reset_emo_type() -> void:
 	if (MgrNft.is_rpg404() or MgrNft.is_strxngers()) and MgrNft.NFT_TRAITS.emo:
-		var res = "res://texture/emo/%s.png" % [MgrNft.NFT_TRAITS.emo]
-		self.texture = load(res)
+		_download_emo_texture()
 	else:
 		push_warning("Wrong NFT emo traits.")
+
+
+func _download_emo_texture() -> void:
+	var image_url = "https://rpg404.com/nft/rpg404/texture/emo/%s.png" % [MgrNft.NFT_TRAITS.emo]
+	K.http_download_texture(HTTPRequest, TMP_RPG404_EMO_FILE, image_url)
+
+
+func _on_HTTPRequest_request_completed(result, response_code, _headers, _body):
+	var texture = K.http_request_completed(result, response_code, TMP_RPG404_EMO_FILE)
+	if texture:
+		self.texture = texture
